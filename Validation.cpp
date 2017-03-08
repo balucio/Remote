e89365de09866b4ac413be6b2ccd1676128a3297
Saul Bertuccio <bsaul@inwind.it>
2017-03-08 08:14:22 +0100

    Le funzioni di validazione sono state inserite in una classe separata

---
+++ Validation.cpp
@@ -0,0 +1,60 @@
+/*
+  Validation.cpp - Validation function
+  Saul bertuccio 5 mar 2017
+  Released into the public domain.
+*/
+
+#include "Validation.h"
+
+boolean Validation::isValidDeviceName(const String &device_name) {
+  return Validation::parseAlphaNumString(device_name, Device::MAX_NAME_LENGTH);
+}
+
+boolean Validation::isValidDeviceKeyName(const String &device_key_name) {
+  return Validation::parseAlphaNumString(device_key_name, Key::MAX_KEY_NAME_LEN);
+}
+
+boolean Validation::isValidDeviceType(const String &device_type) {
+
+  for (int i = 0; i < Device::TYPE_NUM; i++) {
+    if (Device::TYPES[i] == device_type)
+      return true;
+  }
+  return false;
+}
+
+boolean Validation::isValidDeviceKeyPulse(const String &device_key_pulse) {
+
+  return Validation::parseNumericString(device_key_pulse, 9);
+  
+}
+
+boolean Validation::isValidDeviceKeyCode(const String &device_key_code) {
+  
+    return Validation::parseNumericString(device_key_code, 9);
+
+}
+
+boolean Validation::parseAlphaNumString(const String &s, const int l) {
+
+  for(byte i=0; i<s.length(); i++) {
+    if(!isAlphaNumeric(s.charAt(i)))
+      return false;
+  }
+
+  return (s.length() > 0 && s.length() <= l);
+
+}
+
+boolean Validation::parseNumericString(const String &s, const int l) {
+
+  for(byte i=0; i<s.length(); i++) {
+    if(!isDigit(s.charAt(i)))
+      return false;
+  }
+
+  return (s.length() > 0 && s.length() <= l);
+
+}
+
+
---
+++ Validation.h
@@ -0,0 +1,32 @@
+/*
+  Validation.h - Handle validation
+  Saul bertuccio 5 mar 2017
+  Released into the public domain.
+*/
+#ifndef Validation_h
+#define Validation_h
+
+#include "Arduino.h"
+#include "Device.h"
+#include "Key.h"
+
+class Validation {
+  
+  public:
+
+    static boolean isValidDeviceName(const String &device_name);
+    static boolean isValidDeviceType(const String &device_type);
+    static boolean isValidDeviceKeyName(const String &device_key_name);
+    static boolean isValidDeviceKeyPulse(const String &device_key_pulse);
+    static boolean isValidDeviceKeyCode(const String &device_key_code);
+
+  private:
+
+      static boolean parseAlphaNumString(const String &s, const int l);
+      static boolean parseNumericString(const String &s, const int l);
+
+
+};
+
+#endif
+
